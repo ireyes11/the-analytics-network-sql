3a7a14e7848609822443b19fe86e8058382c0bb2
ireyes11 <128448406+ireyes11@users.noreply.github.com>
2023-04-15 22:16:37 -0600

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -5,3 +5,31 @@ where categoria = 'Electro'
 --2
 select * from stg.product_master
 where origen = 'China'
+
+--3
+select * from stg.product_master
+where categoria='Electro'
+order by nombre
+
+--4
+select * from stg.product_master
+where subcategoria='TV'
+and is_active= 'true'
+
+--5
+select * from stg.store_master
+where pais='Argentina'
+order by fecha_apertura asc
+nulls first
+
+--6
+select * from stg.order_line_sale
+order by fecha desc
+limit 5
+
+--7
+select * from stg.super_store_count
+order by cast (fecha as date)
+limit 10
+
+--8
